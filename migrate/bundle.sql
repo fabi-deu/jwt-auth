5cecef142dd5b585cfa58ca124d5f3372ad6bdc9
ju_deu <fabi_deu@icloud.com>
2025-01-02 14:27:00 +0100

    implemented handler for creating new users

--- migrate/bundle.sql
+++ migrate/bundle.sql
@@ -1,13 +1,13 @@
 CREATE TYPE Permission AS ENUM ( 'USER', 'ADMIN' );
 
 CREATE TABLE "users" (
-                         uuid VARCHAR PRIMARY KEY,
-                         username VARCHAR NOT NULL,
-                         email VARCHAR NOT NULL,
-                         password VARCHAR NOT NULL,
+    uuid VARCHAR PRIMARY KEY,
+    username VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
 
-                         permission Permission default 'user',
-                         tokenversion bigint default 1,
+    permission Permission default 'user',
+    tokenserial bigint default 1,
 
-                         timestamp bigint DEFAULT EXTRACT(EPOCH FROM NOW())
+    timestamp bigint DEFAULT EXTRACT(EPOCH FROM NOW())
 )
