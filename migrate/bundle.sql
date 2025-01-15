3978cf153915799cec8aad60efd986f5c656cafb
ju_deu <fabi_deu@icloud.com>
2025-01-15 17:27:43 +0100

    made username UNIQUE

--- migrate/bundle.sql
+++ migrate/bundle.sql
@@ -2,12 +2,12 @@ CREATE TYPE Permission AS ENUM ( 'USER', 'ADMIN' );
 
 CREATE TABLE "users" (
     uuid VARCHAR PRIMARY KEY,
-    username VARCHAR NOT NULL,
+    username VARCHAR NOT NULL UNIQUE,
     email VARCHAR NOT NULL,
     password VARCHAR NOT NULL,
 
-    permission Permission default 'user',
-    tokenid VARCHAR,
+    permission Permission default 'USER',
+    tokenid VARCHAR NOT NULL,
 
     timestamp bigint DEFAULT EXTRACT(EPOCH FROM NOW())
 )
